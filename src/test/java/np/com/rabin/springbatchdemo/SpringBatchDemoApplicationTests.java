372c11a62ddb0c23ecc87c22e487a15c986982d7
Rabin Awal <morbid_leathal@hotmail.com>
2022-04-23 23:30:45 -0500

    Created Spring boot project with Spring batch and created entity class

---
+++ src/test/java/np/com/rabin/springbatchdemo/SpringBatchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package np.com.rabin.springbatchdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
